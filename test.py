287649455d145424c752f79657f46b6ae1c853bd
wuchengkai <50603580+wuchengkai@users.noreply.github.com>
2020-04-22 14:45:52 +0800

    创建test文件
    第一次使用github创建项目文件

---
+++ test.py
@@ -0,0 +1,17 @@
+class MyNumber:
+    def __iter__(self):
+        self.a = 1
+        return self
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+myclass = MyNumber()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
